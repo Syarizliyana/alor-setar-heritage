bb83e682a50a9e1d1e0272e400ac36ecaf61d382
Syarizliyana <syarizqistina@gmail.com>
2026-06-12 16:04:55 +0800

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+
+      manifest: {
+        name: 'Alor Setar Heritage Trail',
+        short_name: 'Heritage Trail',
+        description: 'Explore Kedah Heritage Through AR',
+        theme_color: '#0f766e',
+        background_color: '#0f766e',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
